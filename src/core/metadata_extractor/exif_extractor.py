83432e947ef5c053253caf332eef5561b16ff6f6
CaoHoangTung <caohoangtung2001@gmail.com>
2023-04-04 16:52:55 +0700

    metadata: add metadata in pipeline

--- src/core/metadata_extractor/exif_extractor.py
+++ src/core/metadata_extractor/exif_extractor.py
@@ -1,11 +1,16 @@
-from PIL import Image
 from PIL.ExifTags import TAGS
+from .base import MetadataExtractor
 
-def extract_exif(image_path):
-    with Image.open(image_path) as img:
-        exifdata = img.getexif()
-        metadata = {}
-        for tag_id, value in exifdata.items():
-            tag = TAGS.get(tag_id, tag_id)
-            metadata[tag] = value
+def extract_exif(img):
+    exifdata = img.getexif()
+    metadata = {}
+    for tag_id, value in exifdata.items():
+        tag = TAGS.get(tag_id, tag_id)
+        metadata[tag] = value
     return metadata
+
+
+class ExifMetadataExtractor(MetadataExtractor):
+    def extract_metadata(self, image):
+        metadata = extract_exif(image.copy())
+        return metadata
--- tests/test_metadata_extractor.py
+++ tests/test_metadata_extractor.py
@@ -1,9 +1,9 @@
-from omegaconf import open_dict
-import pytest
+from PIL import Image
 from src.core.metadata_extractor.exif_extractor import extract_exif
 
 def test_exif_extractor():
     exif_image_path = "assets/imgs/img_with_exif.png"
-    extracted_metadata = extract_exif(exif_image_path)
-    actual_metadata = {'ResolutionUnit': 2, 'ExifOffset': 78, 'XResolution': 144.0, 'YResolution': 144.0}
-    assert extracted_metadata == actual_metadata
+    with Image.open(exif_image_path).convert("RGB") as img:
+        extracted_metadata = extract_exif(img)
+        actual_metadata = {'ResolutionUnit': 2, 'ExifOffset': 78, 'XResolution': 144.0, 'YResolution': 144.0}
+        assert extracted_metadata == actual_metadata
