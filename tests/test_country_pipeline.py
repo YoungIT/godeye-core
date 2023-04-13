c3b04b583128bc1f943efe5a6d70f2e6f2e49f31
CaoHoangTung <caohoangtung2001@gmail.com>
2023-04-13 16:38:44 +0700

    fix: add requirements, fix absolute path, fix unit test

--- tests/test_country_pipeline.py
+++ tests/test_country_pipeline.py
@@ -8,7 +8,7 @@ def test_init_country_grid():
     country_grid = CountryGrid(preload=True)
     assert country_grid.num_classes == len(Country)
     for cell in country_grid.get_cells():
-        assert cell.name in [c.value for c in Country]
+        assert cell.repr_cls in [c.value for c in Country]
 
 def test_country_pipeline():
     with initialize(version_base="1.1", config_path="../configs"):
