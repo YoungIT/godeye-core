aae10c1f4423f5ae222cc7f6d2510bda90d6b8d5
LeDuySon <leduyson2632001@gmail.com>
2023-04-18 23:35:01 +0700

    Add: Priotize streetclip in final prediction when have no overlap

--- src/core/common/utils/converter.py
+++ src/core/common/utils/converter.py
@@ -6,7 +6,7 @@ from geopy.geocoders import Nominatim
 
 base_path = pyrootutils.find_root(search_from=__file__, indicator=[".git", "setup.cfg"])
 
-COUNTRY_CONVERTER = json.load(open(os.path.join(base_path, "assets/metadata/country2latlng.json")))
+COUNTRY_CONVERTER = json.load(open(os.path.join(base_path, "assets/metadata/alpha2_latlng.json")))
 CITY_CONVERTER = json.load(open(os.path.join(base_path, "assets/metadata/city2latlng.json")))
 
 ALPHA2_TO_COUNTRY_NAME = {}
